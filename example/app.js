002bb4891e354fab845e7743bbb9f4b27de32cc4
Momme <momme@juergensen.me>
2017-09-04 19:41:09 +0200

    implement bee-queue

---
+++ example/app.js
@@ -0,0 +1,38 @@
+const feathers = require('feathers');
+const rest = require('feathers-rest');
+const socketio = require('feathers-socketio');
+const handler = require('feathers-errors/handler');
+const bodyParser = require('body-parser');
+const memory = require('feathers-memory');
+const plugin = require('../src');
+
+// Create a feathers instance.
+const app = feathers()
+  .configure(socketio())
+  .configure(rest())
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({extended: true}));
+
+app.use('/messages', memory({
+  paginate: {
+    default: 2,
+    max: 4
+  },
+  id:'_id'
+}));
+app.use('/task', plugin({
+  service: app.service('messages'),
+  paginate: {
+    default: 2,
+    max: 4
+  }
+}));
+
+// A basic error handler, just like Express
+app.use(handler());
+
+// Start the server
+var server = app.listen(3030);
+server.on('listening', function () {
+  console.log('Feathers running on 127.0.0.1:3030');
+});
---
+++ src/index.js
@@ -0,0 +1,75 @@
+const Queue = require('bee-queue');
+
+class Service {
+  constructor (options) {
+    this.paginate = options.paginate;
+    this.service = options.service;
+    this.events = ['ready', 'error', 'succeeded', 'retrying', 'failed', 'stalled'];
+    this.queue = new Queue(options.service);
+    this.queue.process((job, done) => this._jobProcess(job, done));
+  }
+
+  setup() {
+    this.queue.on('ready', (result) => this.emit('ready', result));
+    this.queue.on('error', (error) => this.emit('error', error));
+    this.queue.on('succeeded', (job, result) => this.emit('succeeded', {job, result}));
+    this.queue.on('retrying', (job, error) => this.emit('retrying', {job, error}));
+    this.queue.on('failed', (job, error) => this.emit('failed', {job, error}));
+    this.queue.on('stalled', (jobId) => this.emit('stalled', {jobId}));
+  }
+
+  _jobProcess (job, done) {
+    return this.service.create(job.data)
+    .then(res=>done(null, res))
+    .catch(err=>done(err))
+  }
+
+  _parseJob (job) {
+    return {
+      options:job.options,
+      id:job.id,
+      data:job.data,
+      status:job.status
+    }
+  }
+
+  find (params) {
+    let limit = params.query.$limit || this.paginate.default;
+    limit = (limit > this.paginate.max) ? this.paginate.max : limit;
+
+    let start = params.query.$skip || 0;
+    let end = start+limit;
+    let size = limit;
+    return this.queue.getJobs(params.query.type || 'waiting', {start, end, size})
+    .then(res => res.map((val) => this._parseJob(val)))
+    .then(data => ({
+      data,
+      limit,
+      skip: start
+    }))
+  }
+
+  get (id, params) {
+    return new Promise((resolve, reject) => {
+      this.queue.getJob(id, (err, job) => {
+        if(err) return reject(err);
+        resolve(this._parseJob(job));
+      })
+    });
+  }
+
+  create (data, params) {
+    if (Array.isArray(data)) {
+      return Promise.all(data.map(current => this.create(current)));
+    }
+    return this.queue.createJob(data).save()
+    .then(job=>this._parseJob(job))
+  }
+}
+
+function init (options) {
+  return new Service(options);
+}
+
+init.Service = Service;
+module.exports = init;
